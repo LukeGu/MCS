fe2b7ff3d432c849ef1860fbcd5024a745c453d7
Luke Gu <mythnan@gmail.com>
2019-06-23 10:40:32 +1000

    initial MCS

---
+++ Marvel-Client-master/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+//import Home from "./page/home/home";
+import HomePage from "./page/home/homePage";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
